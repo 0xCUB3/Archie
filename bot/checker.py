7d0a46926eddef2eeac08aac93ad9087d265ab26
0xCUBE <94565160+0xCUB3@users.noreply.github.com>
2023-07-11 06:38:52 -0400

    Restructure Exception Checker

--- bot/checker.py
+++ bot/checker.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 def check_is_valid_code(url: str) -> bool:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-
     try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+
         script_tag = soup.find("script", string=lambda t: "isValidCode" in str(t))
         is_valid_code = script_tag.text.split("isValidCode")[1]
         is_valid_code_value = is_valid_code[is_valid_code.find(":") + 1:is_valid_code.find(",")].strip()
